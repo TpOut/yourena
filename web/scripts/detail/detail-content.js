21316ff63e4071713b82e3ca9c28f9e814c4e715
lishengjie <416756910@qq.com>
2019-10-05 23:59:39 +0800

    fix: 文档相对路经无法显示的问题
    Signed-off-by: lishengjie <416756910@qq.com>

--- web/scripts/detail/detail-content.js
+++ web/scripts/detail/detail-content.js
@@ -4,27 +4,39 @@ function getRequest() {
     if (url.indexOf("?") !== -1) {
         var str = url.substr(1);
         var strs = str.split("&");
-        for(var i = 0; i < strs.length; i ++) {
+        for (var i = 0; i < strs.length; i++) {
             //现在只有一个参数
-            if(i === 0){
+            if (i === 0) {
                 theRequest = decodeURI(strs[i].split("=")[1])
             }
         }
     }
     return theRequest;
 }
+
 var notePath = getRequest();
 
 //获取最后的名称
 var splits = notePath.split("/");
-var title =  splits[splits.length - 1].split(".")[0];
+var title = splits[splits.length - 1].split(".")[0];
 document.getElementsByTagName("title")[0].innerText = title;
 document.getElementById("title").innerText = title;
 
-Ajax.get(baseUrl+ notePath, function (res) {
+Ajax.get(baseUrl + notePath, function (res) {
     try {
         document.getElementById('content').innerHTML = marked(res);
-    } catch(err) {
+
+        //如果是相对路经，都需要处理一下
+        var allA = document.getElementsByTagName('a');
+        for (var i = 0; i < allA.length; i++) {
+            var a = allA[i];
+            var baseIndex = a.href.indexOf(baseUrl);
+            if (baseIndex >= 0) {
+                var sub = a.href.substring(a.href.indexOf(baseUrl) + baseUrl.length, a.href.length);
+                a.href = "detail-container.html?path=docs/" + sub;
+            }
+        }
+    } catch (err) {
 
     }
-})
+});
