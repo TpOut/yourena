7a9baea0f457aac035ead6b595f70ab24efad4c0
lishengjie <416756910@qq.com>
2020-03-02 20:22:57 +0800

    修改发布
    Signed-off-by: lishengjie <416756910@qq.com>

--- web/scripts/summary/summary.js
+++ web/scripts/summary/summary.js
@@ -5,7 +5,14 @@ var servletName;
 switch (categoryName) {
     case "blog":
         document.getElementById("header_img").src = "/imgs/icons/ic_story.png";
-        document.getElementById("header_title").innerText = "Blog";
+        let title = document.getElementById("header_title");
+        title.innerText = "Blog";
+        let aRef = document.createElement('a');
+        aRef.setAttribute('href',"https://blog.csdn.net/u013867301");
+        aRef.innerText=" -- csdn"
+        aRef.style.fontSize="24px";
+        title.appendChild(aRef)
+
         servletName = "BlogList";
         break
     case "novel":
